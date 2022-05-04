41ba8e0256505a58618242c4fa38681e9cc7f25f
Talia Seada <talias1235@gmail.com>
2022-05-04 14:01:25 +0300

    first question

--- ex3_utils.py
+++ ex3_utils.py
@@ -14,6 +14,7 @@ def myID() -> np.int:
     """
     return 211551601
 
+
 # ---------------------------------------------------------------------------
 # ------------------------ Lucas Kanade optical flow ------------------------
 # ---------------------------------------------------------------------------
@@ -29,7 +30,50 @@ def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10,
     :param win_size: The optical flow window size (odd number)
     :return: Original points [[x,y]...], [[dU,dV]...] for each points
     """
-    pass
+    # create the returned arrays
+    x_y = []
+    u_v = []
+
+    # convert to GRAY scale
+    if im1.ndim == 3:
+        im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        plt.gray()
+    if im2.ndim == 3:
+        im2 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        plt.gray()
+
+    # Compute the gradients I_x and I_y
+    ker = np.array([[1, 0, -1]])
+    Ix = cv2.filter2D(im2, -1, ker, borderType=cv2.BORDER_REPLICATE)
+    Iy = cv2.filter2D(im2, -1, ker.T, borderType=cv2.BORDER_REPLICATE)
+    It = im2 - im1
+
+    for i in range(0, im1.shape[0] - win_size, step_size):
+        for j in range(0, im1.shape[1] - win_size, step_size):
+            # take the window
+            Ix_cut = Ix[i:i + win_size, j:j + win_size].flatten()
+            Iy_cut = Iy[i:i + win_size, j:j + win_size].flatten()
+            It_cut = It[i:i + win_size, j:j + win_size].flatten()
+
+            # create A and b
+            A = np.array([Ix_cut, Iy_cut]).T
+            b = It_cut.T
+
+            # calculate v
+            AtA = A.T.dot(A)
+            AtA_inv = np.linalg.inv(AtA)
+            u, v = AtA_inv.dot(A.T.dot(b))
+
+            # calculate the eigenvalues
+            e1, e2 = np.linalg.eigvals(AtA)
+            eig_max = max(e1, e2)
+            eig_min = min(e1, e2)
+
+            if eig_max / eig_min < 100 and eig_min > 1:
+                x_y.append([j, i])
+                u_v.append([u, v])
+
+    return np.array(x_y), np.array(u_v)
 
 
 def opticalFlowPyrLK(img1: np.ndarray, img2: np.ndarray, k: int,
@@ -144,4 +188,3 @@ def pyrBlend(img_1: np.ndarray, img_2: np.ndarray,
     :return: (Naive blend, Blended Image)
     """
     pass
-
